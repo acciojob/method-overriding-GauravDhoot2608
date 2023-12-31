f7bf95dcdd121437fa2d712eb1f4d1a2ec20c512
Gaurav Dhoot <gauravdhoot17@gmail.com>
2023-12-31 23:41:00 +0530

    Project Committed 1

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -5,4 +5,13 @@ public class B extends A{
     public String meth(){
         return "Method is overridden in Extendend class B";
     }
+
+    public static void main(String[] args) {
+
+        B obj = new B();
+
+        obj.meth();
+
+        obj.meth();
+    }
 }
